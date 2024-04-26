57180cf83255eda937ffeb9634963993f04bbe34
Omer Bresinski <omerbres@Omers-MacBook-Pro.local>
2024-04-27 01:03:46 +0300

    folder refactor

--- api/index.ts
+++ api/index.ts
@@ -1,7 +1,7 @@
 import "dotenv/config";
 import express from "express";
 import cors from "cors";
-import { apiRouter } from "./api";
+import { apiRouter } from "./apiRouter";
 
 const app = express();
 
