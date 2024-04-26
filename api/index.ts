2159d19f2f5f81d7d27fc3de279117d2ce41a88e
Omer Bresinski <omerbres@Omers-MacBook-Pro.local>
2024-04-27 01:26:46 +0300

    removed unnecessary get endpoint

--- api/index.ts
+++ api/index.ts
@@ -8,10 +8,6 @@ const app = express();
 app.use(cors());
 app.use("/api", apiRouter);
 
-app.get("/", (req, res) => {
-  res.send("Hello, Omer Bresinski!");
-});
-
 app.listen(process.env.PORT, () => {
   console.log(`Server is running on http://localhost:${process.env.PORT}`);
 });
