d8ae3a568a619db4a9f5d0804809b268263d9ab6
Omer Bresinski <omerbres@Omers-MacBook-Pro.local>
2024-04-27 01:28:50 +0300

    video uploads

--- api/uploadthing/uploadthing.ts
+++ api/uploadthing/uploadthing.ts
@@ -3,10 +3,14 @@ import { createUploadthing, type FileRouter } from "uploadthing/express";
 const f = createUploadthing();
 
 export const uploadRouter = {
-  imageUploader: f({
+  media: f({
     image: {
       maxFileSize: "4MB",
       maxFileCount: 4,
+    },
+    video: {
+      maxFileSize: "16MB",
+      maxFileCount: 1,
     },
   }).onUploadComplete((data) => {
     console.log("upload completed", data);
